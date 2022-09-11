8c2752d56eb542a81e1123d43b42a37488cc0e76
jeevesh <jeeveshtewatia2@gmail.com>
2022-09-11 22:29:28 +0530

    react fitfth assignment

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductList from "./Components/ProductList";
+function App() {
+  return (
+    <>
+      <ProductList />
+    </>
+  );
+}
+export default App;
---
+++ src/Components/HOC.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+const products = [
+  { productName: "Jeevesh", price: 100 },
+  { productName: "Ajay", price: 9999 },
+  { productName: "vaishno", price: 8888 },
+  { productName: "Sachin", price: 7777 },
+  { productName: "lalit", price: 4444 },
+];
+const withSearch = (WrappedComponent) => {
+  class SearchFuctionality extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        search: "",
+      };
+    }
+    updateSearch = (e) => {
+      this.setState({ search: e.target.value });
+    };
+    filterProducts = () => {
+      return products.filter((product) =>
+        product.productName.toLowerCase().includes(this.state.search)
+      );
+    };
+    render() {
+      const updatedProducts = this.filterProducts();
+      return (
+        <div>
+          <input type="text" onChange={this.updateSearch} />
+          <WrappedComponent products={updatedProducts}></WrappedComponent>
+        </div>
+      );
+    }
+  }
+  return SearchFuctionality;
+};
+export default withSearch;
